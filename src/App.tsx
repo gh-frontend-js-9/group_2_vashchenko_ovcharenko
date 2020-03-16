45c92fee3bd202a710a74045c046f37fcda87f69
Anton Ovcharenko <majestiko@ukr.net>
2020-03-16 21:39:36 +0200

    add a simple type of our template on section blog with 2 img

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
-import MainBlogCard from "./components/MainBlogCard/MainBlogCard";
 import BlogsSection from "./components/BlogsSection/BlogsSection";
+import BlogsWithImage from "./components/BlogsWithImage/BlogsWithImage";
+
 
 function App() {
   return (
     <div className="App">
-     <BlogsSection/>
+     {/*<BlogsSection/>*/}
+    <BlogsWithImage/>
     </div>
   );
 }
